9926174611497758b8eff5c69300d37f1c7b7e9f
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-10-03 14:02:08 -0700

    Chapter 2

--- chapter1.rkt
+++ chapter1.rkt
@@ -1,15 +1,25 @@
 #lang plai-typed
+(print-only-errors #t)
 
 (define-type MisspelledAnimal
 	     [yacc (height : number)]
 	     [caml (humps : number)])
 
-(define ma1 : MisspelledAnimal (yacc 3))
-(define ma2 : MisspelledAnimal (caml 2))
+(define ma1 : MisspelledAnimal (caml 2))
+(define ma2 : MisspelledAnimal (yacc 1.9))
 
 (define (good? [ma : MisspelledAnimal]) : boolean
   (type-case MisspelledAnimal ma
     [caml (humps) (>= humps 2)]
     [yacc (height) (> height 2.1)]))
 
-(good? (yacc 1))
+(test (good? ma1) #t)
+(test (good? ma2) #f)
+
+(define (good2? [ma : MisspelledAnimal]) : boolean
+  (cond
+    [(caml? ma) (>= (caml-humps ma) 2)]
+    [(yacc? ma) (> (yacc-height ma) 2.1)]))
+
+(test (good2? ma1) #t)
+(test (good2? ma2) #f)
