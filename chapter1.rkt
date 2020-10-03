8f130059043338cbe78581b2804387e5ce6e416c
Ricky Clarkson <ricky.clarkson@gmail.com>
2020-10-03 12:43:55 -0700

    Fixed syntax issue.

--- chapter1.rkt
+++ chapter1.rkt
@@ -8,10 +8,8 @@
 (define ma2 : MisspelledAnimal (caml 2))
 
 (define (good? [ma : MisspelledAnimal]) : boolean
-  (
-   type-case MisspelledAnimal ma
-   (
-    [yacc (height) (>= height 2.1)]
-    [caml (humps) (>= humps 2)])))
+  (type-case MisspelledAnimal ma
+    [caml (humps) (>= humps 2)]
+    [yacc (height) (> height 2.1)]))
 
 (good? (yacc 1))
